7598a9f74c6457208000cd2337839d3fdbffc215
DommSite <johan.johansson06@live.se>
2024-02-12 11:03:47 +0100

    Add project files.

---
+++ inlämningsuppgift6lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace inlämningsuppgift6lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in de varor du ska köpa");
+            List <string> varor = new List<string> ();
+            string[] varorr = Console.ReadLine().Split(" ");
+            Console.WriteLine("Skriv in priset för varje vara");
+            string[] pris = Console.ReadLine().Split(" ");
+
+            Console.WriteLine("Skriv in din handlingslista");
+            string[] antalvaror = Console.ReadLine().Split(" ");
+            antalvaror.ToImmutableSortedDictionary();
+            for (int i = 0; i < antalvaror.Length; i++) 
+            {
+                Console.WriteLine(antalvaror[i]);
+            }
+        }
+
+        static string [] läsnamn()
+        {
+        string text = Console.ReadLine();
+        string[] ord = text.Split(" ");
+        return ord;
+        }
+    }
+
+
+}
